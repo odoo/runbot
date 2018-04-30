da2cc0fd9c59437e3095a81187db5cecdbd16800
Moisés López <moylop260@vauxoo.com>
2018-04-30 09:11:29 -0500

    [REF] runbot: Clean directories after finish tests

--- runbot/tests/test_subject_skip_build.py
+++ runbot/tests/test_subject_skip_build.py
@@ -18,40 +18,47 @@ class TestRunbotSkipBuild(TransactionCase):
         if os.path.isdir(hooks_dir):
             # Avoid run a hooks for commit commands
             shutil.rmtree(hooks_dir)
+        self.repo = self.env["runbot.repo"]
 
     def tearDown(self):
         super().tearDown()
         shutil.rmtree(self.tmp_dir)
+        if self.repo and os.path.isdir(self.repo.path):
+            shutil.rmtree(self.repo.path)
+        for build in self.env["runbot.build"].search([("repo_id", "in", self.repo.ids)]):
+            build_dir = os.path.join(self.repo._root(), build.dest)
+            if os.path.isdir(build_dir):
+                shutil.rmtree(build_dir)
 
     def git(self, *cmd):
         subprocess.call(["git"] + list(cmd), cwd=self.work_tree)
 
     def test_subject_skip_build(self):
         """Test [ci skip] feature"""
-        repo = self.env["runbot.repo"].create({"name": self.git_dir})
+        self.repo = self.repo.create({"name": self.git_dir})
 
         cimsg = "Testing subject [ci skip]"
         self.git("commit", "--allow-empty", "-m", cimsg)
-        repo._update_git()
+        self.repo._update_git()
         build = self.env["runbot.build"].search([("subject", "=", cimsg)])
         self.assertFalse(build)
 
         cimsg = "Testing subject without ci skip"
         self.git("commit", "--allow-empty", "-m", cimsg)
-        repo._update_git()
+        self.repo._update_git()
         build = self.env["runbot.build"].search([("subject", "=", cimsg)])
         self.assertTrue(build)
 
         cimsg = "Testing body\n\n[ci skip]"
         self.git("commit", "--allow-empty", "-m", cimsg)
-        repo._update_git()
+        self.repo._update_git()
         build = self.env["runbot.build"].search([
             ("subject", "=", cimsg.split("\n")[0])])
         self.assertFalse(build)
 
         cimsg = "Testing body without\n\nci skip"
         self.git("commit", "--allow-empty", "-m", cimsg)
-        repo._update_git()
+        self.repo._update_git()
         build = self.env["runbot.build"].search([
             ("subject", "=", cimsg.split("\n")[0])])
         self.assertTrue(build)
