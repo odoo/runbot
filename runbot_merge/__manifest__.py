a15ab1c481e1d58b1c675c8badd4d23c33b45bab
Xavier Morel <xmo@odoo.com>
2018-08-27 15:28:22 +0200

    [FIX] runbot_merge: work with admin[uid=2]
    * correctly fetch the admin's uid when running remote tests
    * looks like I had forgotten to add any sort of access rights, so add
      some

--- runbot_merge/__manifest__.py
+++ runbot_merge/__manifest__.py
@@ -2,6 +2,9 @@
     'name': 'merge bot',
     'depends': ['contacts', 'website'],
     'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+
         'data/merge_cron.xml',
         'views/res_partner.xml',
         'views/mergebot.xml',
