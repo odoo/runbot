face7d6d7834472e2d098dbcf55fd4273e2f3ef2
Christophe Monniez <moc@odoo.com>
2019-01-12 17:00:19 +0100

    [FIX] runbot: use regular branches to test last resort
    To test the last resort branch matching when nothing in common can be
    found, two PR were used leading to PR's target branch as the default
    one.
    
    Also, the test was never run beacause of a bad indentation.
    
    With this commit, the indentation is fixed and the test uses regular
    branches.

--- runbot/tests/test_build.py
+++ runbot/tests/test_build.py
@@ -185,31 +185,21 @@ class Test_Build(common.TransactionCase):
         })
         self.assertEqual((server_repo.id, server_pr.target_branch_name, 'default'), addons_build._get_closest_branch_name(server_repo.id))
 
-@patch('odoo.addons.runbot.models.repo.runbot_repo._github')
-def test_closest_branch_05_master(self, mock_github):
-    """ test last resort value when nothing common can be found"""
-    mock_github.return_value = {
-        'head' : {'label': 'foo-dev:bar_branch'},
-        'base' : {'ref': 'saas-14'},
-        'state': 'open'
-    }
-    server_repo = self.Repo.create({'name': 'bla@example.com:foo-dev/bar', 'token':  '1'})
-    addons_repo = self.Repo.create({'name': 'bla@example.com:ent-dev/bar', 'token': '1'})
-    server_pr = self.Branch.create({
-        'repo_id': server_repo.id,
-        'name': 'refs/pull/123456'
-    })
-    mock_github.return_value = {
-        'head' : {'label': 'foo-dev:foobar_branch'},
-        'base' : {'ref': '10.0'},
-        'state': 'open'
-    }
-    addons_pr = self.Branch.create({
-        'repo_id': addons_repo.id,
-        'name': 'refs/pull/789101'
-    })
-    addons_build = self.Build.create({
-        'branch_id': addons_pr.id,
-        'name': 'd0d0caca0000ffffffffffffffffffffffffffff',
-    })
-    self.assertEqual((server_repo.id, 'master', 'default'), addons_build._get_closest_branch_name(server_repo.id))
+    def test_closest_branch_05_master(self):
+        """ test last resort value when nothing common can be found"""
+        server_repo = self.Repo.create({'name': 'bla@example.com:foo-dev/bar', 'token':  '1'})
+        addons_repo = self.Repo.create({'name': 'bla@example.com:ent-dev/bar', 'token': '1'})
+        server_pr = self.Branch.create({
+            'repo_id': server_repo.id,
+            'name': 'refs/heads/10.0'
+        })
+        addons_pr = self.Branch.create({
+            'repo_id': addons_repo.id,
+            'name': 'refs/head/badref-fix-foo'
+        })
+        addons_build = self.Build.create({
+            'branch_id': addons_pr.id,
+            'name': 'd0d0caca0000ffffffffffffffffffffffffffff',
+        })
+
+        self.assertEqual((server_repo.id, 'master', 'default'), addons_build._get_closest_branch_name(server_repo.id))
