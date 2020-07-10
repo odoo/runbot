01f54b8249181bb5d0976aca6454bb19b784c16c
Xavier Morel <xmo@odoo.com>
2020-07-10 10:21:43 +0200

    [IMP] runbot_merge: split status configuration into own object
    Having the required statuses be a mere list of contexts has become a
    bit too limiting for our needs as it doesn't allow e.g. adding new
    required statuses on only some branches of a repository (e.g. only
    master), nor does it allow putting checks on only branches, or only
    stagings, which would be useful for overridable checks and the like,
    or for checks which only make sense linked to a specific revision
    range (e.g. "incremental" linting which would only check whatever's
    been modified in a PR).
    
    Split the required statuses into a separate set of objects, any of
    which can be separately marked as applying only to some branches (no
    branch = all branches).
    
    Fixes #382

---
+++ runbot_merge/tests/test_by_branch.py
@@ -0,0 +1,96 @@
+import pytest
+
+from utils import Commit
+
+
+@pytest.fixture
+def repo(env, project, make_repo, users, setreviewers):
+    r = make_repo('repo')
+    project.write({
+        'repo_ids': [(0, 0, {
+            'name': r.name,
+            'status_ids': [
+                (0, 0, {'context': 'ci'}),
+                # require the lint status on master
+                (0, 0, {
+                    'context': 'lint',
+                    'branch_ids': [(4, project.branch_ids.id, False)]
+                }),
+            ]
+        })],
+    })
+    setreviewers(*project.repo_ids)
+    return r
+
+def test_status_applies(env, repo, config):
+    """ If branches are associated with a repo status, only those branch should
+    require the status on their PRs & stagings
+    """
+    with repo:
+        m = repo.make_commits(None, Commit('root', tree={'a': '1'}), ref='heads/master')
+
+        [c] = repo.make_commits(m, Commit('pr', tree={'a': '2'}), ref='heads/change')
+        pr = repo.make_pr(target='master', title="super change", head='change')
+    pr_id = env['runbot_merge.pull_requests'].search([
+        ('repository.name', '=', repo.name),
+        ('number', '=', pr.number)
+    ])
+    assert pr_id.state == 'opened'
+
+    with repo:
+        repo.post_status(c, 'success', 'ci')
+    env.run_crons('runbot_merge.process_updated_commits')
+    assert pr_id.state == 'opened'
+
+    with repo:
+        repo.post_status(c, 'success', 'lint')
+    env.run_crons('runbot_merge.process_updated_commits')
+    assert pr_id.state == 'validated'
+
+    with repo:
+        pr.post_comment('hansen r+', config['role_reviewer']['token'])
+    env.run_crons()
+
+    st = env['runbot_merge.stagings'].search([])
+    assert st.state == 'pending'
+    with repo:
+        repo.post_status('staging.master', 'success', 'ci')
+    env.run_crons('runbot_merge.process_updated_commits')
+    assert st.state == 'pending'
+    with repo:
+        repo.post_status('staging.master', 'success', 'lint')
+    env.run_crons('runbot_merge.process_updated_commits')
+    assert st.state == 'success'
+
+def test_status_skipped(env, project, repo, config):
+    """ Branches not associated with a repo status should not require the status
+    on their PRs or stagings
+    """
+    # add a second branch for which the lint status doesn't apply
+    project.write({'branch_ids': [(0, 0, {'name': 'maintenance'})]})
+    with repo:
+        m = repo.make_commits(None, Commit('root', tree={'a': '1'}), ref='heads/maintenance')
+
+        [c] = repo.make_commits(m, Commit('pr', tree={'a': '2'}), ref='heads/change')
+        pr = repo.make_pr(target='maintenance', title="super change", head='change')
+    pr_id = env['runbot_merge.pull_requests'].search([
+        ('repository.name', '=', repo.name),
+        ('number', '=', pr.number)
+    ])
+    assert pr_id.state == 'opened'
+
+    with repo:
+        repo.post_status(c, 'success', 'ci')
+    env.run_crons('runbot_merge.process_updated_commits')
+    assert pr_id.state == 'validated'
+
+    with repo:
+        pr.post_comment('hansen r+', config['role_reviewer']['token'])
+    env.run_crons()
+
+    st = env['runbot_merge.stagings'].search([])
+    assert st.state == 'pending'
+    with repo:
+        repo.post_status('staging.maintenance', 'success', 'ci')
+    env.run_crons('runbot_merge.process_updated_commits')
+    assert st.state == 'success'
