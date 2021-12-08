460e26bafc5792daab04afa4e456494591989ce5
Xavier-Do <xdo@odoo.com>
2021-12-08 13:50:23 +0100

    [IMP] runbot: add populated data

---
+++ runbot_populate/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "runbot demo",
+    'summary': "Runbot demo data",
+    'description': "Runbot demo data",
+    'author': "Odoo SA",
+    'website': "http://runbot.odoo.com",
+    'category': 'Website',
+    'version': '1.0',
+    'depends': ['runbot'],
+    'demo': [
+        'demo/runbot_demo.xml',
+    ],
+    'license': 'LGPL-3',
+}
