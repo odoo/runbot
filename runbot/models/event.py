02ed33c4987fa40be6b8029f2793c480e3236a36
Xavier-Do <xdo@odoo.com>
2022-12-15 13:38:55 +0100

    [FIX] runbot: keep log create date

--- runbot/models/event.py
+++ runbot/models/event.py
@@ -17,6 +17,12 @@ class runbot_event(models.Model):
 
     _inherit = "ir.logging"
     _order = 'id'
+    _log_access = False
+
+    create_uid = fields.Many2one('res.users', string='Created by', automatic=True, readonly=True)
+    create_date = fields.Datetime(string='Created on', automatic=True, readonly=True)
+    write_uid = fields.Many2one('res.users', string='Last Updated by', automatic=True, readonly=True)
+    write_date = fields.Datetime(string='Last Updated on', automatic=True, readonly=True)
 
     build_id = fields.Many2one('runbot.build', 'Build', index=True, ondelete='cascade')
     active_step_id = fields.Many2one('runbot.build.config.step', 'Active step', index=True)
@@ -26,6 +32,12 @@ class runbot_event(models.Model):
 
     @api.model_create_multi
     def create(self, vals_list):
+        for vals in vals_list:
+            vals.setdefault('write_uid', self.env.uid)
+            vals.setdefault('write_date', self.env.cr.now())
+            vals.setdefault('create_uid', self.env.uid)
+            vals.setdefault('create_date', self.env.cr.now())
+
         logs_by_build_id = defaultdict(list)
         for log in vals_list:
             if 'build_id' in log:
@@ -41,6 +53,11 @@ class runbot_event(models.Model):
                 elif ir_log['level'].upper() == 'ERROR':
                     build.triggered_result = 'ko'
         return super().create(vals_list)
+
+    def write(self, vals):
+        vals.setdefault('write_uid', self.env.uid)
+        vals.setdefault('write_date', self.env.cr.now())
+        super().write(vals)
 
     def _markdown(self):
         """ Apply pseudo markdown parser for message.
