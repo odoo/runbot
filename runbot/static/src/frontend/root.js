2f1120249d3b14583b1a3881a69a6ef272bfb257
William Braeckman <wbr@odoo.com>
2025-03-10 15:56:44 +0100

    [IMP] runbot: add basic owl structure
    Adds the minimal requirements for owl and xml file based templating for
    the frontend.

---
+++ runbot/static/src/frontend/root.js
@@ -0,0 +1,64 @@
+import { whenReady, Component, xml, App, onError } from '@runbot/owl';
+
+import { getTemplate } from '@web/core/templates';
+import { registry } from '@web/core/registry';
+import { useRegistry } from '@web/core/registry_hook';
+
+
+const mainComponents = registry.category('main.components');
+
+class ErrorHandler extends Component {
+    static template = xml`<t t-slot="default" />`;
+    static props = ["onError", "slots"];
+    setup() {
+        onError((error) => {
+            this.props.onError(error);
+        });
+    }
+}
+
+class ComponentContainer extends Component {
+    static components = { ErrorHandler };
+    static props = {};
+    static template = xml`
+    <div class="o-main-components-container">
+        <t t-foreach="Components.entries" t-as="C" t-key="C[0]">
+            <ErrorHandler onError="error => this.handleComponentError(error, C)">
+                <t t-component="C[1].Component" t-props="C[1].props"/>
+            </ErrorHandler>
+        </t>
+    </div>
+    `;
+
+    setup() {
+        this.Components = useRegistry(mainComponents);
+    }
+
+    handleComponentError(error, C) {
+        console.error('Error while rendering', C[0], 'removing from app.');
+        // remove the faulty component and rerender without it
+        this.Components.entries.splice(this.Components.entries.indexOf(C), 1);
+        this.render();
+        /**
+         * we rethrow the error to notify the user something bad happened.
+         * We do it after a tick to make sure owl can properly finish its
+         * rendering
+         */
+        Promise.resolve().then(() => {
+            throw error;
+        });
+    }
+}
+
+/**
+ * Bootstrap the frontend.
+ */
+(async function startApp() {
+    await whenReady();
+
+    const app = new App(ComponentContainer, {
+        getTemplate,
+        env: {},
+    });
+    await app.mount(document.body);
+})();
