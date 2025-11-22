095f6378f97ae6cff4f4f6936b956a4090e8342f
AnhBT <anhbtit@gmail.com>
2025-11-22 13:36:08 +0700

    [FIX] runbot_merge: Wrong module import and remove extra argument on method override.

--- runbot_merge/models/mail_thread.py
+++ runbot_merge/models/mail_thread.py
@@ -1,19 +1,19 @@
 from collections import ChainMap
 
 from odoo import models
-from odoo.tools import ConstantMapping
+from odoo.tools.misc import ConstantMapping
 
 
 class MailThread(models.AbstractModel):
     _inherit = 'mail.thread'
 
-    def _message_compute_author(self, author_id=None, email_from=None, raise_on_email=True):
+    def _message_compute_author(self, author_id=None, email_from=None):
         if author_id is None and self:
             mta = self.env.cr.precommit.data.get(f'mail.tracking.author.{self._name}', {})
             authors = self.env['res.partner'].union(*(p for r in self if (p := mta.get(r.id))))
             if len(authors) == 1:
                 author_id = authors.id
-        v = super()._message_compute_author(author_id, email_from, raise_on_email)
+        v = super()._message_compute_author(author_id, email_from)
         return v
 
     def _track_set_author(self, author, *, fallback=False):
