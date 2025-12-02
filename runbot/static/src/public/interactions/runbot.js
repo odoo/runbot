8fa7299097c991e1767dbe8d6d162a5ed1e0a808
Pierre Paridans <app@odoo.com>
2025-12-02 18:09:07 +0100

    [REF] runbot: frontend with Interaction/Component
    This commit is a massive rewrite to modernize the Runbot frontend's JS
    using Interaction and OWL Components.
    
    In a nutshell:
    - no more jQuery
    - lint/format files (no more global variables, const/let...)
    - Bootstrapify a bit the stats page layout
    - add setup to use Interactions (requirements in assets bundle, start
      services...)
    - rewrite [data-runbot] links and copy button handling using Interaction
    - rewrite stats page using Interaction + Component for the chart/legend

---
+++ runbot/static/src/public/interactions/runbot.js
@@ -0,0 +1,54 @@
+import { Interaction } from "@web/public/interaction";
+import { registry } from "@web/core/registry";
+
+export class Runbot extends Interaction {
+    static selector = "body";
+
+    dynamicContent = {
+        "[data-runbot]": {
+            "t-on-click.prevent": this.onDataRunbotClick,
+        },
+        "[data-clipboard-copy]": {
+            "t-on-click": this.onClipboardCopy,
+        },
+    };
+
+    async onDataRunbotClick({ currentTarget }) {
+        const { runbot: operation, runbotBuild } = currentTarget.dataset;
+        if (!operation) {
+            return;
+        }
+        let { href: url } = currentTarget;
+        if (runbotBuild) {
+            url = `/runbot/build/${runbotBuild}/${operation}`;
+        }
+        const response = await fetch(url, { method: "POST" });
+        const responseText = await response.text();
+        const { href: currentURL, pathname } = window.location;
+        switch(operation) {
+            case "rebuild":
+                if (pathname.endsWith(`/build/${runbotBuild}`)) {
+                    const redirectURL = new URL(currentURL);
+                    redirectURL.pathname = redirectURL.pathname.replace(`/build/${runbotBuild}`, `/build/${responseText}`);
+                    window.location.href = redirectURL.toString();
+                }
+                break;
+            case "action":
+                currentTarget.parentElement.innerText = responseText;
+                break;
+            default:
+                window.location.reload();
+        }
+    }
+
+    onClipboardCopy({ currentTarget }) {
+        if (!navigator.clipboard) {
+            // eslint-disable-next-line no-console
+            console.warn("Clipboard not supported");
+            return;
+        }
+        navigator.clipboard.writeText(currentTarget.dataset.clipboardCopy);
+    }
+}
+
+registry.category("public.interactions").add("runbot", Runbot);
