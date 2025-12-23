f77c1dbc64a74c3a3d5c2de4848e5289a7bf4182
Xavier-Do <xdo@odoo.com>
2025-12-23 10:57:44 +0100

    [FIX] runbot: fix status service last success update

--- runbot_builder/status.py
+++ runbot_builder/status.py
@@ -5,7 +5,8 @@ from tools import RunbotClient, run
 class StatusClient(RunbotClient):
 
     def loop_turn(self):
-        self.env['runbot.commit.status']._send_to_process()
+        with self.env['runbot.runbot']._manage_host_exception(self.host):
+            self.env['runbot.commit.status']._send_to_process()
         self.env.cr.commit()
         return 5
 
