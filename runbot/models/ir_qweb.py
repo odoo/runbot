e153f03e75c5a4030d217cf24e8a4066b60de3f1
Xavier-Do <xdo@odoo.com>
2026-03-25 08:26:40 +0100

    [IMP] runbot: speedup dev=xml when using dev=xml on runbot the page loading is verry slow (~20 seconds) This is mainly due to an intensive usage of tcalls to generate all small elements in the page. Whith dev=xml, the template is read and recompiled for each call. Using the transaction cache instaed will highy reduce the page loading time (from 20 second to 0.8 second) while allowing to reload the templates at each page loading.

--- runbot/models/ir_qweb.py
+++ runbot/models/ir_qweb.py
@@ -1,5 +1,5 @@
-from ..common import s2human, s2human_long, precise_s2human
-from odoo import models
+from ..common import s2human, s2human_long, precise_s2human, transactioncache
+from odoo import models, tools
 from odoo.http import request
 from odoo.addons.website.controllers.main import QueryURL
 
@@ -12,3 +12,10 @@ class IrQweb(models.AbstractModel):
         values['s2human_long'] = s2human_long
         values['precise_s2human'] = precise_s2human
         return response
+
+    @tools.conditional(
+        'xml' in tools.config['dev_mode'],
+        transactioncache,
+    )  # replace ormcache by transaction cache to avoid reading the same template multiple times in the same requests. Context is ignored but should be the same for each call in the same request
+    def _generate_code_cached(self, ref: int):
+        return super()._generate_code_cached(ref)
