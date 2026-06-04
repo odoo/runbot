2abb74ac741d82ae4215da711f6cfda55530f8cf
Pierre Paridans <app@odoo.com>
2026-06-04 14:19:35 +0200

    [IMP] runbot: revamp the freeze page as a grouped table
    This commit improves the readability and usability of the freeze page :
    - use a more structured grouped table paradigm to provide more cohesive
      alignment and readability.
    - add collapse/expand teams' PRs.
    - add per team counters
    - add collapse/expand all
    - add tags/"done" filtering
    
    To support those changes, the TableGroup and TableFilter js components
    have been added, providing a generic addition on top of Bootstrap's
    classes.
    
    Co-authored-by: Christophe Monniez <moc@odoo.com>

---
+++ runbot/static/src/js/table_group.js
@@ -0,0 +1,59 @@
+// @odoo-module ignore
+
+class TableGroup {
+    static selector = ".table-group";
+    static groupSelector = ".table-group-divider";
+    static groupHeaderSelector = "tr:has(> th)";
+    static groupRowSelector = "tr:not(:has(> th))";
+    static collapseGroupsSelector = "[data-toggle='table-group-collapse']";
+    static hiddenGroupClass = "table-group-hidden";
+
+    constructor(el) {
+        this.el = el;
+        this.expanded = !this.isAllCollapsed;
+        for (const group of this.groups) {
+            const header = this.groupHeader(group);
+            header.querySelector("th").addEventListener("click", () => this.toggleGroup(group));
+        }
+        this.toggleCollapseText();
+        this.collapseButton.addEventListener("click", () => this.toggleCollapse());
+    }
+
+    get groups() {
+        return [...this.el.querySelectorAll(this.constructor.groupSelector)];
+    }
+
+    get collapseButton() {
+        return this.el.querySelector(this.constructor.collapseGroupsSelector);
+    }
+
+    get isAllCollapsed() {
+        return this.groups.every((group) => group.classList.contains(this.constructor.hiddenGroupClass));
+    }
+
+    groupHeader(group) {
+        return group.querySelector(this.constructor.groupHeaderSelector);
+    }
+
+    toggleGroup(group, force = false) {
+        group.classList.toggle(this.constructor.hiddenGroupClass, force ? true : undefined);
+    }
+
+    toggleCollapse() {
+        this.expanded = !this.expanded;
+        for (const group of this.groups) {
+            this.toggleGroup(group, !this.expanded);
+        }
+        this.toggleCollapseText();
+    }
+
+    toggleCollapseText() {
+        this.collapseButton.textContent = this.expanded ? "Collapse all" : "Expand all";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    for (const table of [...document.querySelectorAll(TableGroup.selector)]){
+        new TableGroup(table);
+    }
+});
