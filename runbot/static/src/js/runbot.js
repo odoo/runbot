510947021da85f8b3434399bb9f4d9a204e0c803
Pierre Paridans <app@odoo.com>
2026-08-14 12:42:27 +0200

    [REF] runbot: convert [data-runbot] click handler to vanilla JS
    Replace the jQuery click delegation in `runbot.js` with a native DOM
    event listener.
    
    Note: `document.addEventListener` call doesn't need to wait for
    DOMContentLoaded event, hence removing the equivalent `$.ready` wrapper
    (`$(function () { ... })`).

--- runbot/static/src/js/runbot.js
+++ runbot/static/src/js/runbot.js
@@ -1,34 +1,33 @@
 // @odoo-module ignore
-(function($) {
-    "use strict";   
-    $(function () {
-        $(document).on('click', '[data-runbot]', function (e) {
-            e.preventDefault();
-            var data = $(this).data();
-            var operation = data.runbot;
-            if (!operation) { 
-                return; 
-            }
-            var xhr = new XMLHttpRequest();
-            var url = e.target.href
-            if (data.runbotBuild) {
-                url = '/runbot/build/' + data.runbotBuild + '/' + operation
-            }
-            var elem = e.target 
-            xhr.addEventListener('load', function () {
-                if (operation == 'rebuild' && window.location.href.split('?')[0].endsWith('/build/' + data.runbotBuild)){
-                    window.location.href = window.location.href.replace('/build/' + data.runbotBuild, '/build/' + xhr.responseText);
-                } else if (operation == 'action') {
-                    elem.parentElement.innerText = this.responseText
-                } else {
-                    window.location.reload();
-                }
-            });
-            xhr.open('POST', url);
-            xhr.send();
-        });
+
+document.addEventListener("click", function (e) {
+    const elem = e.target.closest("[data-runbot]");
+    if (!elem) {
+        return;
+    }
+    e.preventDefault();
+    const data = elem.dataset;
+    var operation = data.runbot;
+    if (!operation) {
+        return;
+    }
+    var xhr = new XMLHttpRequest();
+    let url = elem.href;
+    if (data.runbotBuild) {
+        url = '/runbot/build/' + data.runbotBuild + '/' + operation
+    }
+    xhr.addEventListener('load', function () {
+        if (operation == 'rebuild' && window.location.href.split('?')[0].endsWith('/build/' + data.runbotBuild)){
+            window.location.href = window.location.href.replace('/build/' + data.runbotBuild, '/build/' + xhr.responseText);
+        } else if (operation == 'action') {
+            elem.parentElement.innerText = this.responseText
+        } else {
+            window.location.reload();
+        }
     });
-})(jQuery);
+    xhr.open('POST', url);
+    xhr.send();
+});
 
 document.addEventListener('click', function (e) {
     const button = e.target.closest('[data-copy-text]');
