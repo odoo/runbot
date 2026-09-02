odoo/runbot

commit 4c245ac9e8c2f251c121db7c6c7e18b9662ab8a0

[IMP] runbot: limit history graph's width to 100%

runbot/static/src/js/fields/history_graph.js
@@ -3,8 +3,8 @@ import { useRef, xml, Component, useEffect } from "@odoo/owl";
 
 export class HistoryGraph extends Component {
     static template = xml`
-        <div class="w-100">
-            <canvas t-ref="canvas"/>
+        <div>
+            <canvas class="w-100" t-ref="canvas"/>
         </div>
     `;
 

commit 8fa2c57afafc59164ecfa40b7f553f4b0483ac7a

[FIX] runbot: limit history too wide in medium screen size
This reverts commit odoo/runbot@4c245ac9e8c2f251c121db7c6c7e18b9662ab8a0
which introduced a regression by the graph being blurry and the cell
mouse's hovering not being properly adjusted on the right side of the
graph.

This commit fixes the inital issue by giving the graph the whole
column's width, avoiding it to overflow (while allowing its container to
overflow if it doesn't fit).

runbot/static/src/js/fields/history_graph.js
@@ -3,8 +3,8 @@ import { useRef, xml, Component, useEffect } from "@odoo/owl";
 
 export class HistoryGraph extends Component {
     static template = xml`
-        <div>
-            <canvas class="w-100" t-ref="canvas"/>
+        <div class="w-100 overflow-x-auto">
+            <canvas t-ref="canvas"/>
         </div>
     `;
 

runbot/views/build_error_views.xml
@@ -63,7 +63,8 @@
                   <field name="last_seen_date" widget="frontend_url" options="{'link_field': 'last_seen_build_id', 'numeric': True}"/>
                   <field name="first_seen_build_id" invisible="True"/>
                   <field name="last_seen_build_id" invisible="True"/>
-                  <field name="history_data" widget="history_graph" options="{'cell_size': 13, 'mouse_actions': True}"/>
+                  <label for="history_data" colspan="2"/>
+                  <field name="history_data" widget="history_graph" options="{'cell_size': 13, 'mouse_actions': True}" colspan="2" nolabel="1"/>
                   <button name="action_appearing_batches" string="Appearing Batches" type="object" class="btn btn-primary" invisible="not first_seen_batch_ids"/>
                   <button name="action_disappearing_batches" string="Disappearing Batches" type="object" class="btn btn-primary" invisible="not disappearing_batch_ids"/>
                 </group>